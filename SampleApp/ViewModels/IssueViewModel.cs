267ac1fcb0c9498a0491897799e9676992301783
kbfriston3 <okaryu569@gmail.com>
2024-08-27 15:10:47 +0900

    The name of the property to be bound to SelectedItem in the ComboBox must never be gItemh.

---
+++ SampleApp/ViewModels/IssueViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.ViewModels
+{
+    public class IssueViewModel : Bases.ViewModelBase
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Subject { get; set; }
+        public ObservableCollection<IssueViewModel> Children { get; set; }
+
+        public bool IsExpanded { get; set; } = true;
+
+        public ReactivePropertySlim<bool> IsExpandedRP { get; set; }
+
+        public IssueViewModel(int id, string subject)
+        {
+            Id = id;
+            Subject = subject;
+            Children = new ObservableCollection<IssueViewModel>();
+
+            this.ObserveProperty(a => a.IsExpanded).Subscribe(a =>
+            {
+                // Do something...
+            });
+
+            IsExpandedRP = new ReactivePropertySlim<bool>(true).AddTo(disposables);
+            //IsExpanded.Subscribe(ie =>
+            //{
+            //});
+        }
+
+        public void ExpandAll()
+        {
+            IsExpanded = true;
+            //IsExpandedRP.Value = true;
+            foreach (var c in Children)
+            {
+                c.ExpandAll();
+            }
+        }
+
+        public void CollapseAll()
+        {
+            foreach (var c in Children)
+            {
+                c.CollapseAll();
+            }
+            //IsExpandedRP.Value = false;
+            IsExpanded = false;
+        }
+    }
+}
---
+++ SampleApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using SampleApp.Attribute;
+using SampleApp.Converters;
+using SampleApp.Properties;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleApp.ViewModels
+{
+    public class MainWindowViewModel : Bases.ViewModelBase
+    {
+        public ReactiveCommand TestCommand { get; set; }
+        public List<string> Items { get; set; }
+        public string Item { get; set; } = "default";
+
+        public MainWindowViewModel()
+        {
+            Items = new List<string>() { "aaa", "bbb", "ccc", "default" };
+
+            TestCommand = new ReactiveCommand().WithSubscribe(() =>
+            {
+                Items = new List<string> { "1", "2", "3" };
+            });
+        }
+    }
+}
